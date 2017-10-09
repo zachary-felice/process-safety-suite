03649625dae097685121465a08beae8358a52e61
zachary-felice <zachary.a.felice@gmail.com>
2017-10-09 13:48:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+from distutils.core import setup
 
+setup(
+  name='ProcessSafetySuite',
+  version='0.1dev',
+  packages=['cpss',],
+  license='MIT License',
+  long_description=open('README.md').read(),
+)
